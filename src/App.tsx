22153f261ea1a432f4163ef1e975144e840e7d1e
mdeport <mateo.deport@ecole-isitech.fr>
2024-08-28 20:13:00 +0200

    add cards into principal page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import { Card } from "./components/Card";
+import movies from "./movies.json";
+import "./App.css";
 
 function App() {
   return (
     <main>
       <Navbar />
       <div className="container bg-light"></div>
-      <Card />
+      <div className="cards-container">
+        {movies.map((movie, index) => (
+          <Card key={index} title={movie.Title} year={movie.Year} genre={movie.Genre} director={movie.Director} plot={movie.Plot} poster={movie.Poster} />
+        ))}
+      </div>
     </main>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import { FC } from "react";
 
-const Card = () => {
+
+type IProps = {
+  title: string;
+  year: string;
+  genre: string;
+  director: string;
+  plot: string;
+  poster: string;
+};
+
+const Card: FC<IProps> = ({ title, year, genre, director, plot, poster }) => {
   return (
     <div className="card" style={{ width: "18rem" }}>
-      <img src="..." className="card-img-top" alt="..." />
+      <img src={poster} className="card-img-top" alt={title} />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 className="card-title">
+          {title} ({year})
+        </h5>
+        <p className="card-text">
+          <strong>Genre:</strong> {genre}
+        </p>
+        <p className="card-text">
+          <strong>Director:</strong> {director}
+        </p>
+        <p className="card-text">{plot}</p>
       </div>
     </div>
   );
