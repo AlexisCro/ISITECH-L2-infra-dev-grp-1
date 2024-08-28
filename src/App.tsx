824859da8e3b51d3d3f0c5799f388134beae581b
mdeport <mateo.deport@ecole-isitech.fr>
2024-08-28 17:17:30 +0200

    add cards just from  bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Card } from "./components/Card";
 
 function App() {
   return (
-		<main>
-			<Navbar />
-			<div className="container bg-light"></div>
-		</main>
-	);
+    <main>
+      <Navbar />
+      <div className="container bg-light"></div>
+      <Card />
+    </main>
+  );
 }
 
 export default App;
