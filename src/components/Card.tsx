b629e3b9af51de1d3ef6d33560f9af09d1442bd3
clemberard <clement.berard@ecole-isitech.fr>
2024-08-29 14:36:20 +0200

    fix some problems in readme.md and indentation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,59 +16,59 @@ type IProps = {
 };
 
 const Card: FC<IProps> = (props) => {
-  const [showDetails, setShowDetails] = useState(false);
-  const { title, year, genre, director, plot, poster, movie, favorites, setFavoriteMovies } = props;
-  const ShowDetail = () => {
-    setShowDetails(!showDetails);
-  };
+	const [showDetails, setShowDetails] = useState(false);
+	const { title, year, genre, director, plot, poster, movie, favorites, setFavoriteMovies } = props;
+	const ShowDetail = () => {
+		setShowDetails(!showDetails);
+	};
 
-  const addFavorite = (movie: movie) => {
-    const newFavoriteMovies = [...favorites, movie];
+	const addFavorite = (movie: movie) => {
+		const newFavoriteMovies = [...favorites, movie];
 
 		setFavoriteMovies(newFavoriteMovies);
 	};
 
-  const removeFavorite = (movie: movie) => {
-    const newFavoriteMovies = favorites.filter((favorites) => favorites.Title !== movie.Title);
-    
-    setFavoriteMovies(newFavoriteMovies);
-  };
+	const removeFavorite = (movie: movie) => {
+		const newFavoriteMovies = favorites.filter((favorites) => favorites.Title !== movie.Title);
 
-  return (
-		<div className="col d-flex justify-content-center">
-    <div className="card" style={{ width: "18rem" }}>
-      <img src={poster} className="card-img-top" alt={title} />
-      <div className="card-body">
-        <h5 className="card-title">
-          {title} ({year})
-        </h5>
-        <p className="card-text">
-          <strong>Genre:</strong> {genre}
-        </p>
-        <p className="card-text">
-          <strong>Director:</strong> {director}
-        </p>
-        {showDetails && (
-          <div className="card-details">
-            <p className="card-text">
-              <strong>Detail :</strong> {plot}
-            </p>
-          </div>
-        )}
-      </div>
-      <div className="card-footer d-flex justify-content-between">
-        <Favorite
-          favorites={favorites}
-          movie={movie}
-          onClick={() => {
-            favorites.filter((favorites) => favorites.Title === movie.Title && favorites.Year === movie.Year).length > 0 ? removeFavorite(movie) : addFavorite(movie);
-          }}
-        />
-        <button onClick={ShowDetail} className="btn btn-primary">
-          {showDetails ? "Hide Details" : "Show Details"}
-        </button>
-      </div>
-    </div>
+		setFavoriteMovies(newFavoriteMovies);
+	};
+
+	return (
+		<div className="col d-flex justify-content-center py-3">
+			<div className="card" style={{ width: "18rem" }}>
+				<img src={poster} className="card-img-top" alt={title} />
+				<div className="card-body">
+					<h5 className="card-title">
+						{title} ({year})
+					</h5>
+					<p className="card-text">
+						<strong>Genre:</strong> {genre}
+					</p>
+					<p className="card-text">
+						<strong>Director:</strong> {director}
+					</p>
+					{showDetails && (
+						<div className="card-details">
+							<p className="card-text">
+								<strong>Detail :</strong> {plot}
+							</p>
+						</div>
+					)}
+				</div>
+				<div className="card-footer d-flex justify-content-between">
+					<Favorite
+						favorites={favorites}
+						movie={movie}
+						onClick={() => {
+							favorites.filter((favorites) => favorites.Title === movie.Title && favorites.Year === movie.Year).length > 0 ? removeFavorite(movie) : addFavorite(movie);
+						}}
+					/>
+					<button onClick={ShowDetail} className="btn btn-primary">
+						{showDetails ? "Hide Details" : "Show Details"}
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
