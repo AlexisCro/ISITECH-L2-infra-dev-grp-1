2b126d42f4c3386a9c3a79f4f08e15584021ae17
mdeport <mateo.deport@ecole-isitech.fr>
2024-08-30 18:39:42 +0200

    resolv conflict

---
+++ src/App.unit.test.tsx
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("saves and retrieves item from localStorage", () => {
+  render(<App />);
+
+  const key = "key";
+  const value = "test value";
+  //localStorage.setItem(key, value);
+  const storedValue = localStorage.getItem(key);
+  expect(storedValue).toBe(value);
+});
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,34 +4,34 @@ import { movie } from "../types/movie";
 import { useState } from "react";
 
 type IProps = {
-	title: string;
-	year: string;
-	genre: string;
-	director: string;
-	plot: string;
-	poster: string;
-	movie: movie;
-	favorites: movie[];
-	setFavoriteMovies: (movies: movie[]) => void;
+  title: string;
+  year: string;
+  genre: string;
+  director: string;
+  plot: string;
+  poster: string;
+  movie: movie;
+  favorites: movie[];
+  setFavoriteMovies: (movies: movie[]) => void;
 };
 
 const Card: FC<IProps> = (props) => {
-	const [showDetails, setShowDetails] = useState(false);
-	const { title, year, genre, director, plot, poster, movie, favorites, setFavoriteMovies } = props;
-	const ShowDetail = () => {
-		setShowDetails(!showDetails);
-	};
+  const [showDetails, setShowDetails] = useState(false);
+  const { title, year, genre, director, plot, poster, movie, favorites, setFavoriteMovies } = props;
+  const ShowDetail = () => {
+    setShowDetails(!showDetails);
+  };
 
-	const addFavorite = (movie: movie) => {
-		const newFavoriteMovies = [...favorites, movie];
+  const addFavorite = (movie: movie) => {
+    const newFavoriteMovies = [...favorites, movie];
 
-		setFavoriteMovies(newFavoriteMovies);
-	};
+    setFavoriteMovies(newFavoriteMovies);
+  };
 
-	const removeFavorite = (movie: movie) => {
-		const newFavoriteMovies = favorites.filter((favorites) => favorites.Title !== movie.Title);
+  const removeFavorite = (movie: movie) => {
+    const newFavoriteMovies = favorites.filter((favorites) => favorites.Title !== movie.Title);
 
-		setFavoriteMovies(newFavoriteMovies);
+    setFavoriteMovies(newFavoriteMovies);
   };
 
   const theme = localStorage.getItem("theme");
@@ -40,43 +40,43 @@ const Card: FC<IProps> = (props) => {
     classCard += " bg-secondary text-white";
   }
 
-	return (
-		<div className="col d-flex justify-content-center py-3">
-			<div className={classCard} style={{ width: "18rem" }}>
-				<img src={poster} className="card-img-top" alt={title} />
-				<div className="card-body">
-					<h5 className="card-title">
-						{title} ({year})
-					</h5>
-					<p className="card-text">
-						<strong>Genre:</strong> {genre}
-					</p>
-					<p className="card-text">
-						<strong>Director:</strong> {director}
-					</p>
-					{showDetails && (
-						<div className="card-details">
-							<p className="card-text">
-								<strong>Detail :</strong> {plot}
-							</p>
-						</div>
-					)}
-				</div>
-				<div className="card-footer d-flex justify-content-between">
-					<Favorite
-						favorites={favorites}
-						movie={movie}
-						onClick={() => {
-							favorites.filter((favorites) => favorites.Title === movie.Title && favorites.Year === movie.Year).length > 0 ? removeFavorite(movie) : addFavorite(movie);
-						}}
-					/>
-					<button onClick={ShowDetail} className="btn btn-primary">
-						{showDetails ? "Hide Details" : "Show Details"}
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="col d-flex justify-content-center py-3">
+      <div className={classCard} style={{ width: "18rem" }}>
+        <img src={poster} className="card-img-top" alt={title} />
+        <div className="card-body">
+          <h5 className="card-title">
+            {title} ({year})
+          </h5>
+          <p className="card-text">
+            <strong>Genre:</strong> {genre}
+          </p>
+          <p className="card-text">
+            <strong>Director:</strong> {director}
+          </p>
+          {showDetails && (
+            <div className="card-details">
+              <p className="card-text">
+                <strong>Detail :</strong> {plot}
+              </p>
+            </div>
+          )}
+        </div>
+        <div className="card-footer d-flex justify-content-between">
+          <Favorite
+            favorites={favorites}
+            movie={movie}
+            onClick={() => {
+              favorites.filter((favorites) => favorites.Title === movie.Title && favorites.Year === movie.Year).length > 0 ? removeFavorite(movie) : addFavorite(movie);
+            }}
+          />
+          <button onClick={ShowDetail} className="btn btn-primary">
+            {showDetails ? "Hide Details" : "Show Details"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { Card };
